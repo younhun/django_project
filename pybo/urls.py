36063f6cd6352767c9ca5e792a5f93a9bb92bba6
yhyh0614 <yhyh0614@gmail.com>
2024-04-22 16:33:48 +0900

    third commit

--- pybo/urls.py
+++ pybo/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    # 질문 클릭하면 해당 질문 페이지 이동
+    path('<int:question_id>/', views.detail),
 ]
--- pybo/views.py
+++ pybo/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
-
 from .models import Question
 # Create your views here.
-
 def index(request): 
+    # 질문 목록 데이터 얻기
+    # order_by는 정렬 -> (-기호가 있으면 내림차순, 없으면 오름차순)
     question_list = Question.objects.order_by('-create_date')
+    # 딕셔너리 형태로 render
     context = {'question_list' : question_list}
+    # 템플릿 파일을 작성하기 전에
+    # 디렉토리 먼저 생성
+    # config/settings.py 파일의 TEMPLATES 항목에 설정
     return render(request,'pybo/question_list.html',context)
+
+def detatil(request, question_id):
+    question = Question.objects.get(id=question_id)
+    context = {'question':question}
+    return render(request,'pybo/question_detail.html',context)
